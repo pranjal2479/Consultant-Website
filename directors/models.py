4250cd00c15652fc3d5d7880486825cb513a4ab4
Pranjal Dilip Hirlekar <116293590+pranjal2479@users.noreply.github.com>
2026-06-03 13:32:03 +0530

    Add files via upload

---
+++ directors/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Director(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    designation = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    image = models.ImageField(upload_to='directors/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
