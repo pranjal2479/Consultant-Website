5daa49eb9f55cf47c5366b3d7127c661277fbca5
Pranjal Dilip Hirlekar <116293590+pranjal2479@users.noreply.github.com>
2026-06-04 14:02:17 +0530

    Update settings.py

--- Crestservedigital/settings.py
+++ Crestservedigital/settings.py
@@ -130,10 +130,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Gmail SMTP
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = 'sales@crestservdigital.com'
-EMAIL_HOST_PASSWORD = 'arud nobk igky igrz'   # ← see note below
-DEFAULT_FROM_EMAIL  = 'Crestserv digital <sales!crestservdigital.com>'  
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pdhirlekar9@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')   # ← see note below
+DEFAULT_FROM_EMAIL = 'pdhirlekar9@gmail.com'
